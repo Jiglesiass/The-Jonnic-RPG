ee7e29376d38417959cdd98b52a696332ca80a6e
Jon Iglesias <jiglesiasseoane@gmail.com>
2017-08-20 20:52:33 +0200

    Camera zoom

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -3,15 +3,25 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
 	public Transform player;
-	private Vector3 offset;
+	public Transform cameraHolder;
 
-	void Start ()
+	[SerializeField]
+	private float zoomSpeed = 3f;
+
+	private void Start()
 	{
-		offset = transform.position - player.position;
+		Camera.main.transform.LookAt(cameraHolder.position + Vector3.up);
 	}
-	
+
 	void Update ()
 	{
-		transform.position = player.position + offset;
+		cameraHolder.rotation = Quaternion.identity;
+
+		float mouseWheel = Input.GetAxis("Mouse ScrollWheel");
+
+		if (mouseWheel != 0)
+		{
+			transform.Translate(Vector3.forward * mouseWheel * zoomSpeed);
+		}
 	}
 }
