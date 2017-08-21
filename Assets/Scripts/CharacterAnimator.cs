b8a327dd56b79576383a7289386fca6f8c0854d8
Yannic <yvaupel@kabelmail.de>
2017-08-21 13:41:46 +0200

    animator+ animator controller changes

--- Assets/Scripts/CharacterAnimator.cs
+++ Assets/Scripts/CharacterAnimator.cs
@@ -6,20 +6,50 @@ using UnityEngine.AI;
 public class CharacterAnimator : MonoBehaviour
 {
 
-	const float locomotionAnimationSmoothTime = .2f;
+    const float locomotionAnimationSmoothTime = .2f;
+
+    public GameObject character;
+    public GameObject shield;
+    public Transform weapon;
+    public Transform rightHand;
 
     private NavMeshAgent agent;
-    private Animator animator;
+    private Animator characterAnimator;
+    private Animator shieldAnimator;
+    private bool swordOut;
 
-	void Start ()
+    void Start()
     {
         agent = GetComponent<NavMeshAgent>();
-        animator = GetComponentInChildren<Animator>();
-	}
-	
-	void Update ()
+        characterAnimator = character.GetComponent<Animator>();
+        shieldAnimator = shield.GetComponent<Animator>();
+        swordOut = false;
+    }
+
+    void Update()
     {
         float speedPercent = agent.velocity.magnitude / agent.speed;
-        animator.SetFloat("speedPercent", speedPercent, locomotionAnimationSmoothTime, Time.deltaTime);
-	}
+        characterAnimator.SetFloat("speedPercent", speedPercent, locomotionAnimationSmoothTime, Time.deltaTime);
+
+        if (Input.GetKeyDown(KeyCode.V))
+        {
+            if (swordOut == false)
+            {
+                swordOut = true;
+            }
+            else
+            {
+                swordOut = false;
+            }
+
+            characterAnimator.SetBool("swordOut", swordOut);
+            shieldAnimator.SetBool("swordOut", swordOut);
+        }
+    }
+
+    public void ChangeWeaponParent(GameObject newParent)
+    {
+        weapon.parent = newParent.transform;
+        //  AnimateShield();
+    }
 }
